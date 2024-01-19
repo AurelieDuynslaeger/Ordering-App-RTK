c35450c483259c60dc4849d4fd1506426b61aa8e
AureLeeLi <aduynslaeger@gmail.com>
2024-01-19 19:25:28 +0100

    fix img

--- src/services/products.js
+++ src/services/products.js
@@ -4,43 +4,43 @@ const products = [
         id: 0,
         name : "Margarita",
         price : 7.90,
-        cover: "/pizzas/margarita.jpg"
+        cover: "/pizzas/margarita.png"
     },
     {
         id: 1,
         name : "Reine",
         price : 8.90,
-        cover: "/pizzas/regina.jpg"
+        cover: "/pizzas/regina.png"
     },
     {
         id: 2,
         name : "Pepperoni",
         price : 11.90,
-        cover: "/pizzas/diabola.jpg"
+        cover: "/pizzas/diabola.png"
     },
     {
         id: 3,
         name : "4 Saisons",
         price : 12.90,
-        cover: "/pizzas/4saisons.jpg"
+        cover: "/pizzas/4saisons.png"
     },
     {
         id: 4,
         name : "Proscuitto",
         price : 10.90,
-        cover: "/pizzas/proscuitto.jpg"
+        cover: "/pizzas/proscuitto.png"
     },
     {
         id: 5,
         name : "Alla Marinara",
         price : 13.90,
-        cover: "/pizzas/allaMarinara.jpg"
+        cover: "/pizzas/allaMarinara.png"
     },
     {
         id: 6,
         name : "Funghi e Salsicce",
         price : 13.90,
-        cover: "/pizzas/funghiSalsicce.jpg"
+        cover: "/pizzas/funghiSalsicce.png"
     },
 ]
 
